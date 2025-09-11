4fa78220ec9609f9070b154474c14562530b2f55
Rémi Rivière <remi.riviere.free@gmail.com>
2025-09-11 18:05:07 +0200

    Improve filter class

--- frontend/src/components/organisms/ProjectListBlock/ProjectListBlock.tsx
+++ frontend/src/components/organisms/ProjectListBlock/ProjectListBlock.tsx
@@ -56,12 +56,12 @@ const ProjectListBlock: React.FC<ProjectListBlockProps> = ({
 
 
   const handleChange = (e: any) => {
-    setActiveFilters({...activeFilters, project: e?.value})
+    setActiveFilters({ ...activeFilters, project: e?.value })
   }
 
   const handleClick = (e: any) => {
-    const filterValue = e.currentTarget.getAttribute("filtervalue") as string
-    const thematic = e.currentTarget.getAttribute("thematic")
+    const filterValue = e.value as string
+    const thematic = e.getAttribute("data-thematic")
     if (thematic) {
       activeFilters.thematics.includes(filterValue)
         ? setActiveFilters({ ...activeFilters, thematics: activeFilters.thematics.filter((filter) => filter !== filterValue) })
@@ -78,48 +78,51 @@ const ProjectListBlock: React.FC<ProjectListBlockProps> = ({
       className={clsx('width-[100vw] mx-0 pt-md pb-sm', className)}
       {...props}
     >
-      {title && <Title variant="medium" className="mb-sm text-white" level={titleLevel}>
-        {title}
-      </Title>}
+      <div className='container'>
 
-      <div className='flex w-full flex-col md:flex-row'>
-        <div className='flex w-full md:w-2/3 gap-xs flex-col justify-center md:justify-start md:pl-10 my-md'>
-          <Button variant="secondary" color='violet' className={clsx('w-max hover:bg-building', hideFilters ? 'not-rotate-arrow' : 'rotate-arrow')} onClick={() => setHideFilters(!hideFilters)}> Filtres </Button>
-          <div className='flex w-full gap-xs flex-col md:flex-row flex-wrap transition-base' style={{ "visibility": hideFilters ? 'hidden' : "visible", "opacity": hideFilters ? '0' : '1', "height": hideFilters ? '0' : 'auto' }}>
-            {filters?.map((filter, index) => (
-              <Filter key={index} {...filter} checked={activeFilters.tags.concat(activeFilters.thematics).includes(filter.filterValue)} onClick={handleClick} />
-            ))}
+        {title && <Title variant="medium" className="mb-sm text-white" level={titleLevel}>
+          {title}
+        </Title>}
+
+        <div className='flex w-full flex-col md:flex-row'>
+          <div className='flex w-full md:w-2/3 gap-xs flex-col justify-center md:justify-start my-md'>
+            <Button variant="secondary" color='violet' className={clsx('w-max hover:bg-building', hideFilters ? 'not-rotate-arrow' : 'rotate-arrow')} onClick={() => setHideFilters(!hideFilters)}> Filtres </Button>
+            <div className='flex w-full gap-xs flex-col md:flex-row flex-wrap transition-base' style={{ "visibility": hideFilters ? 'hidden' : "visible", "opacity": hideFilters ? '0' : '1', "height": hideFilters ? '0' : 'auto' }}>
+              {filters?.map((filter, index) => (
+                <Filter key={index} {...filter} checked={activeFilters.tags.concat(activeFilters.thematics).includes(filter.filterValue)} onClick={handleClick} />
+              ))}
+            </div>
           </div>
+
+          <div className='flex w-full md:w-1/2 justify-center md:justify-end gap-xs flex-row flex-wrap md:pr-10 my-md'>
+            <SearchInput searchFilter={activeFilters.project} handleChange={handleChange} />
+          </div>
+
         </div>
 
-        <div className='flex w-full md:w-1/2 justify-center md:justify-end gap-xs flex-row flex-wrap md:pr-10 my-md'>
-          <SearchInput searchFilter={activeFilters.project} handleChange={handleChange} />
+
+        <div className="flex gap-xs justify-center flex-wrap">
+          {displayProjects?.sort((a, b) => (new Date(b.date).valueOf() - new Date(a.date).valueOf())).map((project, index) => (
+            <ProjectListCard
+              key={index}
+              project={project.project}
+              link={project.link}
+              association={project.association}
+              description={project.description}
+              thematics={project.thematics}
+              image={project.image}
+            />
+          ))}
         </div>
 
-      </div>
+        <div className='flex justify-center my-md'>
+          <Pagination pageCount={Math.ceil(filteredProjects.length / pageSize)} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+        </div>
 
 
-      <div className="flex gap-xs justify-center flex-wrap">
-        {displayProjects?.sort((a, b) => (new Date(b.date).valueOf() - new Date(a.date).valueOf())).map((project, index) => (
-          <ProjectListCard
-            key={index}
-            project={project.project}
-            link={project.link}
-            association={project.association}
-            description={project.description}
-            thematics={project.thematics}
-            image={project.image}
-          />
-        ))}
-      </div>
-
-      <div className='flex justify-center my-md'>
-        <Pagination pageCount={Math.ceil(filteredProjects.length / pageSize)} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      </div>
-
-
-      <div className='flex justify-center my-md'>
-        <Button href={"/todo"} color="white" hasArrow> Devenir bénévole sur un projet </Button>
+        <div className='flex justify-center my-md'>
+          <Button href={"/todo"} color="white" hasArrow> Devenir bénévole sur un projet </Button>
+        </div>
       </div>
     </div>
   );
