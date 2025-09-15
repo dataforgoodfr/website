997091ea89be2cdc5f8f8b8a2f64532f8c402d5f
Rémi Rivière <remi.riviere.free@gmail.com>
2025-09-15 17:20:41 +0200

    Open partner in new tab and smaller logos

--- frontend/src/components/molecules/PartnerCard/PartnerCard.tsx
+++ frontend/src/components/molecules/PartnerCard/PartnerCard.tsx
@@ -31,12 +31,13 @@ const PartnerCard: React.FC<PartnerCardProps> = ({
         'shadow-block shadow-block--black w-full h-full',
         className,
       )}
+      target="_blank" rel="noreferrer" 
       href={link}
       aria-label={name}
       {...props}
     >
       <div className="flex flex-col sm:flex-row items-center bg-white h-full">
-        <Image loading="lazy" src={image} alt="" width={157} height={157} className="w-[157px] h-[157px] object-contain" />
+        <Image loading="lazy" src={image} alt="" width={157} height={157} className="w-[157px] h-[157px] object-contain p-5" />
         <div className="flex-1 flex flex-col justify-center gap-xs py-10 px-5">
           <Title level={titleLevel} variant="x-small">{name}</Title>
           {description && <p>{description}</p>}
