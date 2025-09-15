74d6e6b68ec1d144829529b100c46c519393bd1a
Thomas Catinaud Taris <thomas@catinaud-taris.fr>
2025-09-15 09:27:19 +0200

    fixed: masked bg

--- frontend/src/components/molecules/LargeTextImage/LargeTextImage.tsx
+++ frontend/src/components/molecules/LargeTextImage/LargeTextImage.tsx
@@ -34,37 +34,49 @@ const LargeTextImage: React.FC<LargeTextImageProps> = ({
     return null;
   }
 
-  const bgImageClass = background === "gray" ? 'bg-[url("/images/bg-highlight-gray.svg")]' : 'bg-[url("/images/bg-highlight-purple.svg")]';
+  const bgImageClass = background === "gray" ? 'bg-[url("/images/bg-highlight-gray.png")]' : 'bg-[url("/images/bg-highlight-purple.png")]';
 
   return (
     <div
-      className={clsx(
-        'overflow-hidden grid grid-cols-1 grid-rows-[200px_1fr] md:grid-rows-1 max-h-[850px]',
-        background === "gray" ? "text-black bg-gray-100" : "text-white bg-building",
-        className,
-      )}
+      className={className}
+      style={{
+        // Get real image
+        maskImage: 'url(/images/mask-papper.svg)',
+        maskSize: 'cover',
+        maskRepeat: 'no-repeat',
+        maskPosition: 'center',
+        WebkitMaskImage: 'url(/images/mask-papper.svg)',
+        WebkitMaskSize: 'cover',
+        WebkitMaskRepeat: 'no-repeat',
+        WebkitMaskPosition: 'center',
+      }}
       {...props}
     >
-      {image && <Image src={image} alt="" loading="lazy" width={1000} height={400} className="col-start-1 row-start-1 w-full h-full object-cover" />}
-      
-      <div className="col-start-1 row-start-2 md:row-start-1">
-        <div className="container h-full">
-          <div className={clsx(
-            'flex flex-col justify-center items-start max-w-xl ml-auto h-full bg-cover bg-center',
-            bgImageClass,
-          )}>
-            {title && <Title level={titleLevel} variant="medium">{title}</Title>}
+      <div
+        className={clsx(
+          'overflow-hidden grid grid-cols-1 grid-rows-[200px_1fr] md:grid-rows-1 max-h-[850px] h-full',
+          background === "gray" ? "text-black bg-gray-100" : "text-white bg-building",
+        )}
+      >
+        {image && <Image src={image} alt="" loading="lazy" width={1000} height={400} className="col-start-1 row-start-1 w-full h-full object-cover" />}
+        
+        <div className="col-start-1 row-start-2 md:row-start-1">
+            <div className={clsx(
+              'flex flex-col justify-center items-start max-w-2xl lg:pl-40 ml-auto h-full bg-cover bg-left',
+              bgImageClass,
+            )}>
+              {title && <Title level={titleLevel} variant="medium">{title}</Title>}
 
-            {content && <div className="prose my-xs max-w-sm">{content}</div>}
-            
-            {citation && <div className="flex flex-wrap items-center gap-5">
-              <div className="w-full font-tertiary text-xl lg:text-3xl">{citation}</div>
-              {citationAuthorImage && <Image src={citationAuthorImage} alt="" width={70} height={70} />}
-              {citationAuthor && <div className="flex-1 h4-like">{citationAuthor}</div>}
-              </div>}
+              {content && <div className="prose my-xs max-w-sm">{content}</div>}
+              
+              {citation && <div className="flex flex-wrap items-center gap-5">
+                <div className="w-full font-tertiary text-xl lg:text-3xl">{citation}</div>
+                {citationAuthorImage && <Image src={citationAuthorImage} alt="" width={70} height={70} />}
+                {citationAuthor && <div className="flex-1 h4-like">{citationAuthor}</div>}
+                </div>}
 
-            {ctaText && ctaLink && <Button href={ctaLink}>{ctaText}</Button>}
-          </div>
+              {ctaText && ctaLink && <Button href={ctaLink}>{ctaText}</Button>}
+            </div>
         </div>
       </div>
     </div>
