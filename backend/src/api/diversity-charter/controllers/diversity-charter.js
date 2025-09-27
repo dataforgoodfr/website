1c93d2e524cad7cb57b3851d07111e036216dbb8
Legrand Thomas <legrand.thomas5@hotmail.fr>
2025-09-27 09:54:06 +0200

    ``` feat(Content): Add CGU, DemocracyCharter, and FAQ
    Adds the CGU, DiversityCharter, and FAQ content types. ```

---
+++ backend/src/api/diversity-charter/controllers/diversity-charter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * diversity-charter controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::diversity-charter.diversity-charter');
